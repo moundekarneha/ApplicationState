48557ae1caac04bf65bbdd05747635766aca610c
HP <HP@DESKTOP-G4J7E8F>
2022-07-21 17:21:54 +0530

    Add project files.

---
+++ ApplicationState/Demo.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApplicationState
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string s = Application["data"].ToString();
+            Response.Write(s);
+        }
+    }
+}
